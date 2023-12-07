bfb2238373ea1709873e7ee41589592bdd715844
jriverosesma <jriverosema@gmail.com>
2023-12-07 01:34:11 +0100

    fix: missing import headers

--- my_package/__main__.py
+++ my_package/__main__.py
@@ -1,3 +1,4 @@
+# My Package
 from my_package.main import main
 
 if __name__ == "__main__":
--- my_package/main.py
+++ my_package/main.py
@@ -1,4 +1,6 @@
+# My Package
 from my_package import __version__ as my_package_version
+
 
 def main() -> None:
     print(my_package_version)
